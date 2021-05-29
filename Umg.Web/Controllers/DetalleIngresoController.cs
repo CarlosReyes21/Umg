d12ee738e6c1c7781b7c8b30bff1b59fe3a7a4f6
CarlosReyes <creyesa7@miumg.edu.gt>
2021-05-28 23:32:21 -0600

    tarea finalizada

--- Umg.Web/Controllers/DetalleIngresoController.cs
+++ Umg.Web/Controllers/DetalleIngresoController.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Umg.Web.Controllers
+namespace Umg.Web.Controllers 
 {
     public class DetalleIngresoController : Controller
     {
